1c6219a6581ddab95562f341d808fb7d41e868c5
San1357 <san1357@gmail.com>
2021-11-15 22:01:12 +0530

    added functionality of Entry_Gate returning pass

---
+++ Entry_Gate.py
@@ -0,0 +1,17 @@
+
+class Entry:
+
+    def Entry_Appointment_no():
+        pass
+
+
+    def access_Appointment_no_from_reception():
+        pass
+
+
+    def Register():
+        pass
+
+
+    def update_to_Register():
+        pass
