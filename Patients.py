7500dfeff7b587e438bfc7410f52f89acfd0361f
San1357 <san1357@gmail.com>
2021-11-15 22:57:25 +0530

    added functionality of Patients_details  returning pass

---
+++ Patients.py
@@ -0,0 +1,22 @@
+
+class Patients:
+    
+    def Disease():
+        pass
+
+    def which_doctor():
+        pass
+
+    def Appointment_details():
+        pass
+
+    def History_of_Patient():
+        pass
+
+    def Patient_details():
+        pass
+
+
+
+
+     
