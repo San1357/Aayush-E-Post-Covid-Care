95172365e642561db86cfcf921a1c2795a8cecff
San1357 <san1357@gmail.com>
2021-11-15 20:15:00 +0530

    naming Appointment functionality with pass

---
+++ Appointment.py
@@ -0,0 +1,12 @@
+
+
+class Appointments:
+
+    def online_appointments():
+        pass
+
+    def visit_appointments():
+        pass
+
+    def Emergency_appointments():  #Immediate appointment
+        pass
