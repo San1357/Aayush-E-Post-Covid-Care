508f2ba4e4b7ab26df6fe52433cf6e6a3a6ba901
San1357 <san1357@gmail.com>
2021-11-15 21:27:31 +0530

    added functionality of checkup room returning pass

---
+++ Checkup_Room.py
@@ -0,0 +1,19 @@
+
+
+class checkup_rooms():
+
+    def patient_details():
+
+        pass
+
+    def test_checkups():
+        pass
+
+
+    def Result_of_test_checkup():
+        pass
+
+    def suggestion_advices():
+        pass
+
+    
